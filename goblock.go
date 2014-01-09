96bf5de24bd90a1a504cef49dcc710f3737b8885
Aaron Bieber <deftly@gmail.com>
2014-01-09 15:10:16 -0700

    there ya go @jrick!

--- goblock.go
+++ goblock.go
@@ -9,24 +9,22 @@ import (
 	"os"
 )
 
-var elog = log.New(os.Stderr, "[!]: ", 0)
-
 func get(src, dest string) (int64, error) {
 	resp, err := http.Get(src)
 	if err != nil {
-		elog.Printf("%v - %v", "Can't make http request", err)
+		log.Fatalf("Can't make http request - %v", err)
 	}
 
 	file, err := os.OpenFile(dest, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
-		elog.Printf("%v - %v", "Can't create file!", err)
+		log.Fatalf("Can't create file! - %v", err)
 	}
 
 	defer resp.Body.Close()
 
 	gz, err := gzip.NewReader(resp.Body)
 	if err != nil {
-		elog.Printf("%v - %v", "Can't uncompress file!", err)
+		log.Fatalf("Can't uncompress file! - %v", err)
 	}
 
 	defer gz.Close()
@@ -40,7 +38,7 @@ func get(src, dest string) (int64, error) {
 func main() {
 	conf, err := goconfig.ParseFile("config.ini")
 	if err != nil {
-		elog.Printf("%v - %v", "Can't parse config file!", err)
+		log.Fatalf("Can't parse config file! - %v", err)
 	}
 
 	url := conf["global"]["url"]
@@ -61,7 +59,7 @@ func main() {
 		log.Printf("downloading %s", key)
 		_, err := get(full_url, file_name)
 		if err != nil {
-			elog.Printf("%v - %v", "Can't write file!", err)
+			log.Fatalf("Can't write file! - %v", err)
 		}
 	}
 }
